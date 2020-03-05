074433a11f309b379a80ab246e4e0009dba10ec6
HAMSANDROS <61355588+HAMSANDROS@users.noreply.github.com>
2020-03-06 00:13:51 +0900

    Add files via upload
    二回目

---
+++ favorist_project_20200306/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <!-- tab title -->
+    <title>favolist</title>
+    <!-- login -->
+    <div class="login_layout">rogin</div>
+</head>
+
+<body class="body">
+ <!-- header start -->
+    <header id="header_layout">
+        <h1 class="logo_layout">favolist</h1>
+        <div id="nav">
+        <ul>
+        <li><a href="index.php">トップページ</a></li>
+        <li><a href="item.html">テスト1</a></li>
+        <li><a href="shop.html">テスト2</a></li>
+        </ul>
+</div>
+    </header>
+ <!-- header end -->
+
+ <!-- footer start -->
+  <footer>
+  <div class="footer_layout">
+    </a>
+    <P>&copy; ..... inc. All Right Reserved.</P>
+    </div>
+  </footer>
+<!-- footer end -->
+    
+</body>
+
+</html>
